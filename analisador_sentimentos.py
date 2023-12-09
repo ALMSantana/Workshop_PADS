6ba58f25f36ac18a80318088be41e34dd323e06e
ALMSantana <andrelms4@insper.edu.br>
2023-12-09 10:53:30 -0300

    Prompt add

---
+++ analisador_sentimentos.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+def analisador_sentimentos(produto):
+    prompt_sistema = f"""
+        Você é um analisador de sentimentos de avaliações de produtos.
+        Escreva um parágrafo com até 50 palavras resumindo as avaliações e 
+        depois atribua qual o sentimento geral para o produto.
+        Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+
+        # Formato de Saída
+
+        Nome do Produto:
+        Resumo das Avaliações:
+        Sentimento Geral: [utilize aqui apenas Positivo, Negativo ou Neutro]
+        Ponto fortes: lista com três bullets
+        Pontos fracos: lista com três bullets
+    """
