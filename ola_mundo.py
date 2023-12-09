74629ad48c89d6a205495333cb72fcc455328887
ALMSantana <andrelms4@insper.edu.br>
2023-12-09 10:46:52 -0300

    Exemplo 1

--- ola_mundo.py
+++ ola_mundo.py
@@ -1,3 +1,4 @@
+
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
@@ -5,3 +6,40 @@ import os
 load_dotenv()
 
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-3.5-turbo"
+
+prompt_sistema = """
+    Assuma que você está classificando sentimentos em reviews.
+    Você deve classificar os sentimentos entre positivo, negativo e
+    neutro.
+
+    Além disso, de justificar, como chegou a este sentimento.
+
+    # Saída esperada
+    Sentimento: [colocar sentimento aqui]
+    Parecer: [colocar parecer aqui]
+
+    # Exemplo
+    Comentário avaliado: Comprei uma base nova da Lush, e a tampa era péssima.
+
+    Sentimento: negativo
+    Parecer: O usuário reclamou da embalagem do produto, sugerindo que era frágil
+"""
+
+resposta = cliente.chat.completions.create(
+    model=modelo,
+    messages=[
+        {
+            "role": "system",
+            "content" : prompt_sistema
+        },
+        {
+            "role" : "user",
+            "content" : "Faça a análise deste comentário: Fiz uma péssima compra"
+        }
+    ]
+)
+
+print("\nResposta: ", resposta.choices[0].message.content)
+
+#https://github.com/ALMSantana/Workshop_PADS
