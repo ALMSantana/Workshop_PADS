03482b7078c56202af248f6212b17eb77ab4d236
ALMSantana <andrelms4@insper.edu.br>
2023-12-09 11:03:12 -0300

    Add exemplo sentimentos

--- analisador_sentimentos.py
+++ analisador_sentimentos.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from tools import carrega
+from tools import salva
 
 load_dotenv()
 
@@ -22,3 +24,29 @@ def analisador_sentimentos(produto):
         Ponto fortes: lista com três bullets
         Pontos fracos: lista com três bullets
     """
+
+    prompt_usuario = carrega(f"dados/avaliacoes-{produto}.txt")
+
+    lista_mensagens = [
+        {
+            "role" : "system",
+            "content" : prompt_sistema
+        },
+        {
+            "role" : "user",
+            "content" : prompt_usuario
+        }
+    ]
+
+    resposta = cliente.chat.completions.create(
+        model=modelo,
+        messages=lista_mensagens
+    )
+    
+    texto_resposta = resposta.choices[0].message.content
+    salva(f"dados/analise-{produto}.txt", texto_resposta)
+
+
+lista_produtos = ["smartphone", "notebook"]
+for nome_produto in lista_produtos:
+    analisador_sentimentos(nome_produto)
